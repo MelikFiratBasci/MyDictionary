d2a837952bb5490ca5fcafd906cbceec36f3a6af
Melik fırat Başçi <mbasci23@hotmail.com>
2021-01-25 19:11:12 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydictionary<int, string> name = new Mydictionary<int, string>();
+            name.Add(13213, "firat");
+            Console.WriteLine(name.Count);
+            name.Print();
+            
+            
+        }
+    }
+}
